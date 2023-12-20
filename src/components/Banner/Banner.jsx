225edda0a28a999cdae465aa1cc2612b56aa2548
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2023-12-21 02:03:06 +0600

    Banner part has been completed

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,22 @@
+import banner from "../../assets/banner.png"
+
+const Banner = () => {
+    return (
+        <div  className="relative w-full ">
+            <img src={banner} className="w-full rounded-lg" />
+            <div className="absolute h-full flex items-center top-0 left-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] rounded-lg ">
+                <div className="w-3/4 md:w-1/2 space-y-1  md:space-y-3  lg:space-y-6 pl-14 ">
+                    <h1 className="text-white text-lg md:text-2xl lg:text-4xl font-bold leading-5">Streamline Your Workflow with our Intuitive Task Manager</h1>
+                    <p className="text-white text-xs md:text-sm  lg:text-lg  "> Effortlessly organize tasks, streamline workflows, and foster teamwork for seamless project management and enhanced efficiency.</p>
+                    <div className="flex gap-5">
+                        <button className="btn  btn-info border-none text-white">Let’s Explore</button>
+                        
+                    </div>
+                </div>
+            </div>
+            
+        </div> 
+    );
+};
+
+export default Banner;
